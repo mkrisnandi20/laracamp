e2cffa25fd11ca75c37a1fbebf9eab45210a0f52
mkrisnandi20 <58869522+mkrisnandi20@users.noreply.github.com>
2021-12-08 09:17:52 +0700

    middleware dashboard, integration admin dashboard

--- resources/views/emails/checkout/afterCheckout.blade.php
+++ resources/views/emails/checkout/afterCheckout.blade.php
@@ -5,8 +5,14 @@ Hi {{ $checkout->User->name }}
 <br>
 Thank you for register on <b>{{ $checkout->Camp->title }}</b>, please see payment intruction by click the button below.
 
-@component('mail::button', ['url' => route('user.checkout.invoice', $checkout->id)])
-Get Invoice
+{{-- use invoice --}}
+{{-- @component('mail::button', ['url' => route('user.checkout.invoice', $checkout->id)]) --}}
+{{-- Get Invoice
+@endcomponent --}}
+
+{{-- no invoice --}}
+@component('mail::button', ['url' => route('dashboard')])
+My Dashboard
 @endcomponent
 
 Thanks,<br>
